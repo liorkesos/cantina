2c0a2e8a0b8d09919223a3a9c9a43227c1f24e57
Tom Bigelajzen <tombigel@bigelx.local>
2010-11-01 21:43:25 +0200

    removed some temps, added html.tpl

--- sites/all/themes/cantina/template.php
+++ sites/all/themes/cantina/template.php
@@ -5,7 +5,7 @@
  * Add body classes if certain regions have content.
  */
 function cantina_preprocess_html(&$variables) {
-	
+
   if (!empty($variables['page']['featured'])) {
     $variables['classes_array'][] = 'featured';
   }
@@ -23,9 +23,6 @@ function cantina_preprocess_html(&$variables) {
     $variables['classes_array'][] = 'footer-columns';
   }
 
-  // Add conditional stylesheets for IE
-  drupal_add_css(path_to_theme() . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
-  drupal_add_css(path_to_theme() . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
 }
 
 /**
--- sites/all/themes/cantina/templates/page.tpl.php
+++ sites/all/themes/cantina/templates/page.tpl.php
@@ -1,38 +1,4 @@
-<!doctype html>
-
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7 ]> <html lang="he" class="no-js ie ie6 lte7 lte8 lte9"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="he" class="no-js ie ie7 lte7 lte8 lte9"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="he" class="no-js ie ie8 lte8 lte9"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="he" class="no-js ie ie9 lte9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html lang="he" class="no-js"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <title>The Cantina</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <!--  Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- CSS : implied media="all" -->
-  <link rel="stylesheet" href="css/style.css?v=2">
-  <link rel="stylesheet" href="css/grid-rtl.css?v=2">
-
-  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
-  <script src="js/libs/modernizr-1.6.min.js"></script>
-
-</head>
-
-<body dir="rtl">
-
-  <div id="container">
-
+<div id="container">
     <header class="region clearfix">
         <div class="container_12">
             <nav class="block grid_10 alpha">
@@ -70,6 +36,7 @@
                 </div>
             </div>
         </section>
+
     <?php if ($page['featured']): ?>
          <section id="content-crousel" class="region clearfix">
                <div class="container_12">
@@ -81,6 +48,7 @@
               </div>
          </section>
      <?php endif; ?>
+
          <div class="container_12">
                 <section id="content-main" class="region grid_7 alpha">
 
@@ -154,31 +122,3 @@
         </div>
     </footer>
   </div> <!--! end of #container -->
-
-
-  <!-- Javascript at the bottom for fast page loading -->
-  <script src="js/libs/jquery-1.4.3.min.js"></script>
-  <script src="js/script.js"></script>
-
-  <!--[if lt IE 7 ]>
-    <script src="js/libs/dd_belatedpng.js"></script>
-    <script> DD_belatedPNG.fix('img, .png_bg'); //fix any <img> or .png_bg background-images </script>
-  <![endif]-->
-
-  <!-- asynchronous google analytics: mathiasbynens.be/notes/async-analytics-snippet
-       change the UA-XXXXX-X to be your site's ID -->
-  <script>
-  /*
-   var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
-   (function(d, t) {
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.async = true;
-    g.src = ('https:' == location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g, s);
-   })(document, 'script');
-  */
-  </script>
-
-</body>
-</html>
